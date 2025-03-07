732a5786b30d6760c0f7bdf055603d7ea828d4a4
mtmlazumlyntontawngyung <mtm.lazumlyntontawngyung@gmail.com>
2025-03-07 09:48:35 +0630

    include constants folder

--- chat-core-api/openapi_server/services/chat_services.py
+++ chat-core-api/openapi_server/services/chat_services.py
@@ -1,13 +1,9 @@
+from openapi_server.variables import responses
+
 
 class ChatServices:
     def generate_chat_response(chat_request):
         bot_style = chat_request.bot_style
-
-        responses = {
-            'normal': "薬を定期的に服用することは重要です。毎日のリマインダーを設定すると役立つかもしれません。",
-            'friend': "大丈夫です、忘れた時は一緒にリマインダーをセットしようね。",
-            'doctor': "薬を飲み忘れると健康に影響を及ぼす可能性があります。リマインダーを使って忘れないようにしましょう。"
-        }
 
         if bot_style not in responses:
             raise ValueError(f"Unsupported bot style: {bot_style}")
