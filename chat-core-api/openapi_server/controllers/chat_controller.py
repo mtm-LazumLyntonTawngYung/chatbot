f1223780e35035e0bfbf1bf0ae4cf03fd31ec70e
mtmlazumlyntontawngyung <mtm.lazumlyntontawngyung@gmail.com>
2025-03-10 16:37:12 +0630

    update controller for chat reply based on the services and update response variables

--- chat-core-api/openapi_server/controllers/chat_controller.py
+++ chat-core-api/openapi_server/controllers/chat_controller.py
@@ -6,22 +6,10 @@ from openapi_server.services.chat_services import ChatServices
 
 
 def reply_chat_case(body=None):  # noqa: E501
-
     if connexion.request.is_json:
-        chat_request = ChatRequest.from_dict(connexion.request.get_json())  # noqa: E501
-
-    try:
-        response_message = ChatServices.generate_chat_response(chat_request)
-
-        chat_respond = ChatRespond(message=response_message)
-        return chat_respond
-
-    except ValueError as e:
-        error_message = f"Error: {str(e)}"
-        chat_respond = ChatRespond(message=error_message)
-        return chat_respond, 400
-
-    except Exception as e:
-        error_message = f"Internal Server Error: {str(e)}"
-        chat_respond = ChatRespond(message=error_message)
-        return chat_respond, 500
+        param_data = ChatRequest.from_dict(connexion.request.get_json())
+        service = ChatServices()
+        ret_msg = service.get_chat_respond(
+            param_data.message, param_data.bot_style)
+        return ChatRespond.from_dict({'message': ret_msg})
+    return None
--- chat-core-api/openapi_server/variables.py
+++ chat-core-api/openapi_server/variables.py
@@ -1,9 +1,24 @@
 import os
 
 responses = {
-    'normal': "薬を定期的に服用することは重要です。毎日のリマインダーを設定すると役立つかもしれません。",
-    'friend': "大丈夫です、忘れた時は一緒にリマインダーをセットしようね。",
-    'doctor': "薬を飲み忘れると健康に影響を及ぼす可能性があります。リマインダーを使って忘れないようにしましょう。"
+    'normal': {
+        'GP_MSG_INTRO': '場合、以下のような対応が良い可能性が高いです。<br><br>',
+        'BP_MSG_INTRO': '<br>反対に、以下のような対応は上の対応方法に比べてあまり良くないようです。<br><br>',
+        'REF_URL_INTRO': '<br>※（）の中はうまくいった確率。<br>詳しくは、こちらをご覧ください。<br>',
+        'NO_DATA_MSG_REPLY': '参考になる情報が見つかりませんでした。<br><br> もしかすると、以下のページで参考になるものがあるかもしれませんので、ぜひご覧ください。<br><a href=https://chienowa-net.com/gpbpsummary target=_blank>https://chienowa-net.com/gpbpsummary</a>',  # noqa: E501
+    },
+    'friend': {
+        'GP_MSG_INTRO': '場合、こんな対応をするといいかも！<br><br>',
+        'BP_MSG_INTRO': '<br>反対に、こんな対応は上の方法と比べるとあまりよくないかもね。<br>',
+        'REF_URL_INTRO': '<br>※（）の中はうまくいった確率。<br>もっと詳しく知りたいなら、これ見てみて！<br>',
+        'NO_DATA_MSG_REPLY': '参考になりそうな情報が見つからなかったよ。<br><br> でも、もしかしたら下のページが役に立つかもしれないから、よかったら見てみて！<br><a href=https://chienowa-net.com/gpbpsummary target=_blank>https://chienowa-net.com/gpbpsummary</a>',  # noqa: E501
+    },
+    'doctor': {
+        'GP_MSG_INTRO': '場合、以下のような対応が適切である可能性が高いです。<br><br>',
+        'BP_MSG_INTRO': '<br>反対に、以下の対応は先に挙げた方法と比べて、適切ではない場合があります。<br>',
+        'REF_URL_INTRO': '<br>※（）の中はうまくいった確率。<br>詳しくは、こちらをご覧ください。<br>',
+        'NO_DATA_MSG_REPLY': '参考となる情報が見つかりませんでした。<br><br> ただ、以下のページに有益な情報があるかもしれませんので、よろしければご覧ください。<br><a href=https://chienowa-net.com/gpbpsummary target=_blank>https://chienowa-net.com/gpbpsummary</a>',  # noqa: E501
+    },
 }
 GEMINI_API_ENDPOINT = os.getenv(
     'GEMINI_API_ENDPOINT', 'https://console-gemini-27920194398.asia-northeast1.run.app')
