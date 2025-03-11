e7fdf306216a21f8f92279ba1fe69839d5c2480e
mtmlazumlyntontawngyung <mtm.lazumlyntontawngyung@gmail.com>
2025-03-11 09:10:38 +0630

    reverse commit due to mistakes update

--- chat-core-api/openapi_server/__main__.py
+++ chat-core-api/openapi_server/__main__.py
@@ -4,9 +4,20 @@ import connexion
 
 from openapi_server import encoder
 from openapi_server.variables import port
+from flask import Flask
 
 
-app = connexion.App(__name__, specification_dir='./openapi/')
-app.app.json_encoder = encoder.JSONEncoder
-app.add_api('openapi.yaml', arguments={'title': 'Chat Core API'}, pythonic_params=True)
-app.run(port=port, debug=True)
+app = Flask(__name__)
+
+
+def main():
+    app = connexion.App(__name__, specification_dir='./openapi/')
+    app.app.json_encoder = encoder.JSONEncoder
+    app.add_api('openapi.yaml',
+                arguments={'title': 'Chat Core Api'},
+                pythonic_params=True)
+
+
+if __name__ == '__main__':
+    app.run(port=port, debug=True)
+    main()
