c9e643ea3e144dfed125730ece8dd5af3ae34ceb
mtmlazumlyntontawngyung <mtm.lazumlyntontawngyung@gmail.com>
2025-03-11 09:02:43 +0630

    update main file for running app according to readme txt

--- chat-core-api/openapi_server/__main__.py
+++ chat-core-api/openapi_server/__main__.py
@@ -4,20 +4,10 @@ import connexion
 
 from openapi_server import encoder
 from openapi_server.variables import port
-from flask import Flask
 
 
-app = Flask(__name__)
-
-
-def main():
-    app = connexion.App(__name__, specification_dir='./openapi/')
-    app.app.json_encoder = encoder.JSONEncoder
-    app.add_api('openapi.yaml',
-                arguments={'title': 'Chat Core Api'},
-                pythonic_params=True)
-
-
-if __name__ == '__main__':
-    app.run(port=port, debug=True)
-    main()
+app = connexion.App(__name__, specification_dir='./openapi/')
+app.app.json_encoder = encoder.JSONEncoder
+app.add_api('openapi.yaml', arguments={
+            'title': 'Chat Core API'}, pythonic_params=True)
+app.run(port=port, debug=True)
